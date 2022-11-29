5032f5cca7f1cc13d81f8c026fc4ce07b205f9ff
Felipe Veloso <felipe@Felipes-MacBook-Pro.local>
2022-11-29 09:54:35 -0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import ChatInput from './ChatInput'
+import MessageList from './MessageList'
+
+function HomePage() {
+  return (
+    <main>
+      <MessageList />
+      <ChatInput />
+    </main>
+  )
+}
+
+export default HomePage
